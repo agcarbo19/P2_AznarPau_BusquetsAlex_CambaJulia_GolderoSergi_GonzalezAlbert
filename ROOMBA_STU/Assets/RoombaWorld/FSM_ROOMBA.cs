d380e2c0c864109c8c7159453968075adcf7d96b
alexbusquets65 <alex.busquets19@gmail.com>
2023-03-11 12:35:36 +0100

    Update

--- ROOMBA_STU/Assets/RoombaWorld/FSM_ROOMBA.cs
+++ ROOMBA_STU/Assets/RoombaWorld/FSM_ROOMBA.cs
@@ -5,18 +5,52 @@ using Steerings;
 [CreateAssetMenu(fileName = "FSM_ROOMBA", menuName = "Finite State Machines/FSM_ROOMBA", order = 1)]
 public class FSM_ROOMBA : FiniteStateMachine
 {
+
+    
+  //  private ROOMBA_Blackboard blackboard;
+    //private GameObject target;
+    
+
     public override void OnEnter()
     {
+
+  //      blackboard = GetComponent<ROOMBA_Blackboard>();
+        
+
+
         base.OnEnter(); // do not remove
     }
 
     public override void OnExit()
     {
+
+
+        base.DisableAllSteerings();
         base.OnExit();
     }
 
     public override void OnConstruction()
     {
+/*
+        State ToPoint = new State ("GoingToPoint",
+        
+            () => { },
+            () => { },
+            () => { }
+        );
 
+
+        Transition changePoint = new Transition("ChangePoint",
+            () => { },
+            () => { }
+        );
+
+        AddStates (ToPoint);
+
+        AddTransition(ToPoint, changePoint, ToPoint);
+
+
+        initialState = ToPoint;
+*/
     }
 }
--- ROOMBA_STU/Assets/RoombaWorld/SceneController.cs
+++ ROOMBA_STU/Assets/RoombaWorld/SceneController.cs
@@ -8,19 +8,29 @@ public class SceneController : MonoBehaviour
     private float maxDustSpawnTimer = 5f;
     private float dustSpawnTimer = 0f;
 
+    /* private Random random1 = new Random();
+    private Random random2 = new Random();
+    private Random random3 = new Random();
+    */
     // Start is called before the first frame update
     void Start()
     {
-
+        /*
+        int n1 = random1.Next(256);
+        int n2 = random2.Next(256);
+        int n3 = random3.Next(256);
+        */
     }
+    
+
 
     // Update is called once per frame
     void Update()
     {
         dustSpawnTimer += Time.deltaTime;
         if (dustSpawnTimer >= maxDustSpawnTimer){
+            //GetComponent<SpriteRenderer>().color = new Color(n1/256, n2/256, n3/256);
             dustSpawn();
-
         }
     }
 
